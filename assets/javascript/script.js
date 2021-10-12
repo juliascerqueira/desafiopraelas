cc3289089c10d5b4c1413a1c601c9b8993239f66
juliascerqueira <juliasscerqueira@outlook.com>
2021-10-11 21:50:59 -0300

    v5 111/10

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,23 @@
 document.getElementById("bunner-button").onclick = function () {
-  location.href = "./pages/cursos/investimento.html";
+  location.href = "/pages/cursos/investimento.html";
 };
 
 // ------------------- Botões curso -------------------------->
 
 document.getElementById("investimento-button").onclick = function () {
-  location.href = "./pages/cursos/investimento.html";
+  location.href = "/pages/cursos/investimento.html";
 };
 
 document.getElementById("independencia-button").onclick = function () {
-  location.href = "./pages/cursos/investimento.html";
+  location.href = "/pages/cursos/investimento.html";
 };
 
 document.getElementById("tradition-button").onclick = function () {
-  location.href = "./pages/cursos/investimento.html";
+  location.href = "/pages/cursos/investimento.html";
 };
 
 // ------------------- Botão blog -------------------------->
 
 document.getElementById("blog-button").onclick = function () {
-  location.href = "./pages/blog/blog.html";
+  location.href = "/pages/blog/blog.html";
 };
